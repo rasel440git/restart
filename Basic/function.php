e817b245b7f3fa0afd5e3f1aef3860a082436350
A.K.M Abdul Hai <abdul.hai@lankabangla.com>
2025-05-13 18:05:44 +0600

    office

--- Basic/function.php
+++ Basic/function.php
@@ -1,9 +1,23 @@
 <?php
 
-function sum() {
-    $value1 = 12;
-    $value2=22;
-    $sum = $value1 + $value2;
+function sum($x=1,$y=2){
+    $x ;
+    $y ;
+   return $sum = $x + $y;
 }
+echo sum(5,3).'</br>';
 
-sum();
+// Varidaic function
+
+function CountryList(...$x){
+    
+   print_r($x)  ;
+}
+ CountryList("bd ","ind","nEP");
+
+
+ // anonoymus function 
+ 
+( function() {
+    echo "Hello world";
+ })();
