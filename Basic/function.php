9e2a8b7263c51b8e05043129c231f35b91974347
A.K.M Abdul Hai <abdul.hai@lankabangla.com>
2025-05-18 18:04:55 +0600

    function

--- Basic/function.php
+++ Basic/function.php
@@ -31,11 +31,49 @@ function sentMail($to='hi', $subject='test', $message= 'body'){
 sentMail();
 
 function ECount() {   
-   static $count= 0;
+    static $count= 0;
    $count ++;
    echo".$count.";
    
 }
 ECount() ;
-ECount() ;
-ECount() ;
+
+//Recursive function
+function recursiveCall($number) {
+   echo "Current Number: " . $number . "\n";
+   $number = $number - 1;
+   if ($number > 0) {
+      recursiveCall($number);
+   }
+}
+//recursiveCall(10);
+
+
+function factorialWithLoop($getNumber){
+   $factorial=1;
+   for( $i= 0; $i<= $factorial; $i++ ) {
+     echo $factorial= $factorial*$i;
+}
+}
+factorialWithLoop(5);
+
+
+//array filter
+
+$number= [12,32,12,3,34,24,55,54,55,45,55,45];
+
+// $filterValue= array_filter($number, function($number) {   
+//     if($number%2==0){
+//       return true;
+//    }
+// });
+
+function arryMod($number) {   
+    if($number>30){
+      return true;
+   }}
+
+$filterValue= array_filter($number, $arryMod
+);
+
+print_r($filterValue);
