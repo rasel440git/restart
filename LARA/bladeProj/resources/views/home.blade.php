5fbdef904c6eae2d75f435b0709e6e5d3cead418
A.K.M Abdul Hai <abdul.hai@lankabangla.com>
2025-08-19 17:45:32 +0600

    bld

---
+++ LARA/bladeProj/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.app')
+
+@section('content')
+    @include('components.hero')
+@endsection
+
+@section('howWeDo')
+    @include('components.howWeDo')
+@endsection
+
+@section('pricing')
+    @include('components.pricing')
+@endsection
+
+@section('team')
+    @include('components.team')
+@endsection
--- LARA/bladeProj/resources/views/layout/app.blade.php
+++ LARA/bladeProj/resources/views/layout/app.blade.php
@@ -7,7 +7,16 @@
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 </head>
 <body>
-    
+@include('layout.header')
+
+    @yield('content')
+    @yield('howWeDo')
+    @yield('pricing')
+    @yield('team')
+
+
+@include('layout.footer')
+
 
     <script src="{{ asset('js/bootstrap/bootstrap.bundle.min') }}"></script>
 </body>
