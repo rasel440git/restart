fc9785315e811631eaeb05804bbf7435bd9ec034
rasel440git <rasel440@gmail.com>
2026-04-28 00:24:01 +0600

    ptc

--- LARA/laratest/app/Http/Controllers/qbController.php
+++ LARA/laratest/app/Http/Controllers/qbController.php
@@ -8,7 +8,30 @@ use Illuminate\Support\Facades\DB;
 class qbController extends Controller
 {
     function demoAction(){
-        $products= DB::table('products')->where('price', '>', 5100)->get();
-        return $products;
+        // $products= DB::table('products')->first();
+        // $products= DB::table('products')->pluck('title', 'id');
+        //  $products= DB::table('products')->count('id');
+        // $products= DB::table('products')->where('price', '>', 5100)->get();
+        //  $products= DB::table('products')->select('id','title','price')->get();
+        //  $products= DB::table('products')->select('title')->distinct()->get();
+
+
+            $products= DB::table('products')
+                      ->join('categories', 'products.category_id', '=', 'categories.id')
+                      ->join('brands', 'products.brand_id', '=', 'brands.id')
+                      ->select('products.id','products.title','categories.categoryName','brands.brandName')
+                      ->get();
+            
+            $productsJoin= DB::table('products')
+                      ->rightJoin('categories', 'products.category_id', '=', 'categories.id')
+                      ->rightJoin('brands', 'products.brand_id', '=', 'brands.id')
+                      ->select('products.id','products.title','categories.categoryName','brands.brandName')
+                      ->get();          
+
+
+
+        // return $products;
+         return response()->json(['products' => $products, 'productsJoin' => $productsJoin]);
+
     }
 }
