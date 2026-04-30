235e0a133fac13a6913b704e0d699d7ae62d1a27
A.K.M Abdul Hai <abdul.hai@lankabangla.com>
2026-04-30 17:03:25 +0600

    ptc

--- LARA/laratest/app/Http/Controllers/qbController.php
+++ LARA/laratest/app/Http/Controllers/qbController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Query\JoinClause;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -16,22 +17,38 @@ class qbController extends Controller
         //  $products= DB::table('products')->select('title')->distinct()->get();
 
 
-            $products= DB::table('products')
-                      ->join('categories', 'products.category_id', '=', 'categories.id')
-                      ->join('brands', 'products.brand_id', '=', 'brands.id')
-                      ->select('products.id','products.title','categories.categoryName','brands.brandName')
-                      ->get();
+            // $products= DB::table('products')
+            //           ->join('categories', 'products.category_id', '=', 'categories.id')
+            //           ->join('brands', 'products.brand_id', '=', 'brands.id')
+            //           ->select('products.id','products.title','categories.categoryName','brands.brandName')
+            //           ->get();
             
-            $productsJoin= DB::table('products')
-                      ->rightJoin('categories', 'products.category_id', '=', 'categories.id')
-                      ->rightJoin('brands', 'products.brand_id', '=', 'brands.id')
-                      ->select('products.id','products.title','categories.categoryName','brands.brandName')
-                      ->get();          
+            // $productsJoin= DB::table('products')
+            //           ->rightJoin('categories', 'products.category_id', '=', 'categories.id')
+            //           ->rightJoin('brands', 'products.brand_id', '=', 'brands.id')
+            //           ->select('products.id','products.title','categories.categoryName','brands.brandName')
+            //           ->get();          
+
+            // $products= DB::table('products')
+            //           ->crossJoin('brands')
+            //           ->get();
+
+
+                // $products= DB::table('products')
+                //           ->join('categories', function (JoinClause $join) {
+                //             $join->on('products.category_id', '=', 'categories.id')
+                //             ->where('categories.categoryName', '=', 'Clothes');                              
+                //             })->get();
+
+                   $insertData= DB::table('brands')->insert([
+                        'brandName' => 'Pepsico',
+                        'brandImg' => 'http://example.com/pepsico.jpg',
+                    ]);
 
 
 
-        // return $products;
-         return response()->json(['products' => $products, 'productsJoin' => $productsJoin]);
+        return $insertData;
+        //  return response()->json(['products' => $products, 'productsJoin' => $productsJoin]);
 
     }
 }
